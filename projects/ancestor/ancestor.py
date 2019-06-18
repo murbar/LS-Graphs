93318ba216d2df940451aff920238fcda54066c3
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-06-18 10:46:50 -0400

    Scaffold ancestor problem

---
+++ projects/ancestor/ancestor.py
@@ -0,0 +1,8 @@
+# unweighted, directed, acyclic graph
+
+
+def earliest_ancestor(graph, vertex):
+    """Returns the ancestor with the farthest distance and lowest numeric ID from the supplied vertex. If none is found, returns -1."""
+
+    earliest = None
+    return earliest
---
+++ projects/ancestor/test_ancestor.py
@@ -0,0 +1,12 @@
+from ancestor import earliest_ancestor
+
+graph = {
+    1: {3},
+    2: {3},
+    3: {6},
+    5: {6, 7},
+    4: {5, 8},
+    8: {9},
+    11: {8},
+    10: {1}
+}
