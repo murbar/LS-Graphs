4d470cb315d47fd06313e0e977490cc9e80abb5f
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-06-18 16:59:17 -0400

    Add Brady's tests

--- projects/ancestor/test_ancestor.py
+++ projects/ancestor/test_ancestor.py
@@ -1,14 +1,42 @@
+import unittest
 from ancestor import earliest_ancestor
 
-graph = {
-    1: {3},
-    2: {3},
-    3: {6},
-    5: {6, 7},
-    4: {5, 8},
-    8: {9},
-    11: {8},
-    10: {1}
-}
 
-print(earliest_ancestor(graph, 6))  # -> 10
+class Test(unittest.TestCase):
+
+    '''
+       10
+     /
+    1   2   4  11
+     \ /   / \ /
+      3   5   8
+       \ / \   \
+        6   7   9
+    '''
+
+    def test_earliest_ancestor(self):
+        graph = {
+            1: {3},
+            2: {3},
+            3: {6},
+            5: {6, 7},
+            4: {5, 8},
+            8: {9},
+            11: {8},
+            10: {1}
+        }
+        self.assertEqual(earliest_ancestor(graph, 1), 10)
+        self.assertEqual(earliest_ancestor(graph, 2), -1)
+        self.assertEqual(earliest_ancestor(graph, 3), 10)
+        self.assertEqual(earliest_ancestor(graph, 4), -1)
+        self.assertEqual(earliest_ancestor(graph, 5), 4)
+        self.assertEqual(earliest_ancestor(graph, 6), 10)
+        self.assertEqual(earliest_ancestor(graph, 7), 4)
+        self.assertEqual(earliest_ancestor(graph, 8), 4)
+        self.assertEqual(earliest_ancestor(graph, 9), 4)
+        self.assertEqual(earliest_ancestor(graph, 10), -1)
+        self.assertEqual(earliest_ancestor(graph, 11), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
