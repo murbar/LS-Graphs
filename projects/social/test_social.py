f7884f00f6dfd8142895f18adb45e2d942f84fae
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-06-19 17:09:42 -0400

    Update tests

--- projects/social/test_social.py
+++ projects/social/test_social.py
@@ -2,8 +2,7 @@ from social import SocialGraph
 import statistics
 
 sg = SocialGraph()
-sg.populateGraph(1000, 10)
-for u, f in sg.friendships.items():
-    print(len(f))
+sg.populateGraph(20, 3)
 lens = [len(f) for u, f in sg.friendships.items()]
+print(sg.friendships)
 print(f"Mean friendships is {statistics.mean(lens)}")
